5505a25e8780cfe8a6f412eba397909fbe8b6d47
B3ryll <b3ryll@proton.me>
2023-03-13 15:36:52 -0300

    v0.1.0+20230313.01: Initial commit
    + Setup webpack svelte compilation with typescript

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let name: string;
+</script>
+
+<main>
+    <h1>hello {name}!</h1>
+</main>
+
+<style>
+    main > h1
+    {
+        color: red;
+    }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const {resolve}            = require("path");
+const HtmlWebpackPlugin    = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const SveltePreprocess     = require("svelte-preprocess");
+
+module.exports = () => {
+    return {
+        mode: "development",
+        entry: {
+            main: "./src/main.ts",
+        },
+        output: {
+            path: resolve(__dirname, "./dist"),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(html|svelte)$/,
+                    use: [
+                        {
+                            loader: 'svelte-loader',
+                            options: {
+                                compilerOptions: {
+                                    dev: true,
+                                },
+
+                                preprocess: SveltePreprocess(),
+                            }, 
+                        },
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                    ]
+
+                },
+                {
+                    test: /\.ts$/,
+                    use: [
+                        "ts-loader",
+                    ],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /node_modules\/svelte\/.*\.mjs$/,
+                    resolve: {
+                        fullySpecified: false,
+                    },
+                },
+            ],
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+            }),
+            new HtmlWebpackPlugin(),
+        ],
+        resolve: {
+            alias: {
+                svelte: resolve('node_modules', 'svelte')
+            },
+            extensions:     ['.mjs', '.js', ".ts", '.svelte'],
+            mainFields:     ['svelte', 'browser', 'module', 'main'],
+            conditionNames: ['svelte'],
+        },
+    };
+};
